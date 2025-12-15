56a6ef07242b6671f16437832351ee07c3d7d5aa
MrMinken <minimoto@hotmail.com>
2025-12-15 22:32:39 +0100

    change in config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  // (valfritt men rekommenderat för SWA)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
