004d33a3d1278aa66633ff37d38872da4627f53c
TinaLutti <143405689+TinaLutti@users.noreply.github.com>
2026-09-04 10:38:45 +0200

    fix: constrain and center login/user/quiz pages on large screens

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -2,8 +2,8 @@ import { QuizSetup } from "../components/quiz/QuizSetup";
 
 export default function page() {
     return (
-        <div className="bg-gray-Page-background">
-            <div className="app-container m-2 text-center font-bold text-xl">
+        <div className="page-wrapper pt-8">
+            <div className="app-container text-center font-bold text-xl max-w-xl mx-auto">
                 Chose What kind of questions you like
                 <QuizSetup />
             </div>
